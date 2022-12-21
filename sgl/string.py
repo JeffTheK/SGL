5b97c7d6b6b6d20dc4bdd85fe73a3d069c96c7a0
JeffTheK <jeff144@protonmail.com>
2022-12-21 16:22:05 +0200

    .

--- sgl/string.py
+++ sgl/string.py
@@ -7,6 +7,14 @@ def _new_str(args, env):
         output += arg.name + " "
     return output[:-1]
 
+def _replace(args, env):
+    old = eval(args[0], env)
+    new = eval(args[1], env)
+    string = eval(args[2], env)
+    new_string = string.replace(old, new)
+    env.vars[args[2]] = new_string
+
 STRING_FUNCS = {
-    Symbol('new-str'): _new_str
+    Symbol('new-str'): _new_str,
+    Symbol('replace'): _replace
 }
--- test/string_test.py
+++ test/string_test.py
@@ -1,5 +1,16 @@
 from sgl.eval import eval_string
 from sgl.std_env import STD_ENV
+from sgl.types import Symbol
+
+def setup_env():
+    env = STD_ENV
+    eval_string('(let str1 (new-str Hello World))', STD_ENV)
+    return env
 
 def test_new_str():
-    assert(eval_string("(new-str Hello World)", STD_ENV) == "Hello World")
+    assert(eval_string("(new-str Hello World)", setup_env()) == "Hello World")
+
+def tests_replace():
+    env = setup_env()
+    eval_string('(replace "World" "Friend" str1)', env)
+    assert(env.vars[Symbol('str1')] == "Hello Friend")
