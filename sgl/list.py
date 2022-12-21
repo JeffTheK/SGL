2d23a84892be40138b349e1715bbb4deb9559150
JeffTheK <jeff144@protonmail.com>
2022-12-21 14:25:55 +0200

    .

--- sgl/list.py
+++ sgl/list.py
@@ -7,6 +7,37 @@ def _new_list(args, env):
         elements.append(eval(arg, env))
     return List(elements)
 
+def _elem_at(args, env):
+    index = eval(args[0], env)
+    list: List = eval(args[1], env)
+    return list.elements[index]
+
+def _pop_at(args, env):
+    index = eval(args[0], env)
+    list: List = eval(args[1], env)
+    return list.elements.pop(index)
+
+def _clear(args, env):
+    list: List = eval(args[0], env)
+    list.elements.clear()
+
+def _len(args, env):
+    list: List = eval(args[0], env)
+    return len(list.elements)
+
+def _for_each(args, env):
+    list: List = eval(args[1], env)
+    iterator = args[0]
+    action = args[2]
+    for elem in list.elements:
+        env.vars[iterator] = elem
+        eval(action, env)
+
 LIST_FUNCS = {
-    Symbol("new-list"): _new_list
+    Symbol('new-list'): _new_list,
+    Symbol('elem-at'): _elem_at,
+    Symbol('pop-at'): _pop_at,
+    Symbol('clear'): _clear,
+    Symbol('len'): _len,
+    Symbol('for-each'): _for_each
 }
--- test/list_test.py
+++ test/list_test.py
@@ -2,7 +2,27 @@ from sgl.eval import eval_string
 from sgl.std_env import STD_ENV
 from sgl.types import List, Symbol
 
+env = STD_ENV
+
 def test_new_list():
-    env = STD_ENV
     eval_string('(let i (new-list 1 "hello" true))', env)
     assert(env.vars[Symbol('i')] == List([1, "hello", True]))
+
+def test_len():
+    assert(eval_string('(len i)', env) == 3)
+
+def test_elem_at():
+    assert(eval_string('(elem-at 2 i)', env) == True)
+
+def test_for_each():
+    eval_string('(let num1 0)', env)
+    eval_string('(for-each x i (let num1 (+ num1 5)))', env)
+    assert(env.vars[Symbol('num1')] == 15)
+
+def test_pop_at():
+    assert(eval_string('(pop-at 2 i)', env) == True)
+    assert(eval_string('(len i)', env) == 2)
+
+def test_clear():
+    eval_string('(clear i)', env)
+    assert(eval_string('(len i)', env) == 0)
