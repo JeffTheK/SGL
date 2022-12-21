8eb3685eca4c4b000aff9bf16a702300a938d487
JeffTheK <jeff144@protonmail.com>
2022-12-21 13:46:55 +0200

    .

--- sgl/std_vars.py
+++ sgl/std_vars.py
@@ -1,6 +1,9 @@
 from .types import Symbol
+from .version import __version__
 
 STD_VARS = {
     Symbol('true'): True,
-    Symbol('false'): False
+    Symbol('false'): False,
+    Symbol('sgl:version'): __version__,
+    Symbol('sgl:authors'): "Dmytro Kolibabchuk"
 }
