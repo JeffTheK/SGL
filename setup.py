af9156bd30e2e5a6771d52a7a911e75b520882a2
JeffTheK <jeff144@protonmail.com>
2022-12-21 14:43:56 +0200

    Add repl

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="sgl",
-    version="1.0.0"
+    version="1.0.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts':
+            ['sgl = sgl.interpreter:run']
+    }
 )
--- sgl/interpreter.py
+++ sgl/interpreter.py
@@ -1,9 +1,10 @@
 from .types import *
 from .lexer import tokenize
 from .parser import parse
-from .eval import eval
+from .eval import eval, eval_string
 from .std_env import STD_ENV
 import sys
+import traceback
 
 def exec_string(string: str):
     tokens = tokenize(string)
@@ -14,10 +15,20 @@ def exec_file(file_path: str):
     exec_string(file.read())
     file.close()
 
+def repl():
+    while True:
+        try: 
+            result = eval_string(input("sgl > "), STD_ENV)
+            print(result)
+        except Exception as e:
+            print("Exception: " + str(e))
+
 def run():
     if len(sys.argv) == 2:
         file_path = sys.argv[1]
         exec_file(file_path)
+    else:
+        repl()
 
 if __name__ == "__main__":
     run()
